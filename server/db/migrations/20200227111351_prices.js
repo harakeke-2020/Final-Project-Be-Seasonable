71ffaf97c79b28d86caceef036499740dc4ea7c8
H-THO <hywel.p.thomas@gmail.com>
2020-02-27 11:20:14 +1300

    add Prices table migrations

---
+++ server/db/migrations/20200227111351_prices.js
@@ -0,0 +1,9 @@
+exports.up = knex =>
+  knex.schema.createTable('prices', (table) => {
+    table.increments('id').primary()
+    table.integer('food_id')
+    table.integer('month')
+    table.double('price')
+  })
+
+exports.down = knex => knex.schema.dropTable('prices')
