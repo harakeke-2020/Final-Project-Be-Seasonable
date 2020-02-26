3901e1a552136b56e90db237184715ca1542bf8f
Hywel Thomas <8552397+H-THO@users.noreply.github.com>
2020-02-27 11:52:31 +1300

    Add Foods seed

--- server/db/migrations/20200227110109_foods.js
+++ server/db/migrations/20200227110109_foods.js
@@ -2,7 +2,7 @@ exports.up = knex =>
   knex.schema.createTable('foods', (table) => {
     table.increments('id').primary()
     table.string('name')
-    table.string('reo-name')
+    table.string('reo_name')
   })
 
 exports.down = knex => knex.schema.dropTable('foods')
--- server/db/seeds/foods.js
+++ server/db/seeds/foods.js
@@ -18,31 +18,31 @@ exports.seed = (knex) =>
 knex('foods').del()
 .then(() => 
   knex('foods').insert([
-{ id: 1, name: 'Apples', 
-{ id: 2, name: 'Avocados',
-{ id: 3, name: 'Bananas', 
-{ id: 4, name: 'Beans',
-{ id: 5, name: 'Broccoli',
-{ id: 6, name: 'Cabbage', 
-{ id: 7, name: 'Capsicum',
-{ id: 8, name: 'Carrots', 
-{ id: 9, name: 'Cauliflower',
-{ id: 10, name: 'Celery', 
-{ id: 11, name: 'Courgette',
-{ id: 12, name: 'Cucumber'
-{ id: 13, name: 'Grapes', 
-{ id: 14, name: 'Kiwifruit',
-{ id: 15, name: 'Kumara', 
-{ id: 16, name: 'Lettuce',
-{ id: 17, name: 'Mandarins',
-{ id: 18, name: 'Mushrooms',
-{ id: 19, name: 'Onions', 
-{ id: 20, name: 'Oranges',
-{ id: 21, name: 'Parsnips'
-{ id: 22, name: 'Pears', 
-{ id: 23, name: 'Pineapple',
-{ id: 24, name: 'Potatoes'
-{ id: 25, name: 'Pumpkin',
-{ id: 26, name: 'Tomatoes'
+{ id: 1, name: 'Apples', reo_name: 'Āporo'},
+{ id: 2, name: 'Avocados', reo_name: 'Awakato'},
+{ id: 3, name: 'Bananas', reo_name: 'Panana'},
+{ id: 4, name: 'Beans', reo_name: 'Pīni'},
+{ id: 5, name: 'Broccoli', reo_name: 'Porokori'},
+{ id: 6, name: 'Cabbage', reo_name: 'Kāpeti'},
+{ id: 7, name: 'Capsicum', reo_name: 'Rapikama'},
+{ id: 8, name: 'Carrots', reo_name: 'Kāroti'},
+{ id: 9, name: 'Cauliflower', reo_name: 'Kareparāoa'},
+{ id: 10, name: 'Celery', reo_name: 'Herewī'},
+{ id: 11, name: 'Courgette', reo_name: 'Roroa iti'},
+{ id: 12, name: 'Cucumber', reo_name: 'Kūkama'},
+{ id: 13, name: 'Grapes', reo_name: 'Kerepi'},
+{ id: 14, name: 'Kiwifruit', reo_name: 'Huakiwi'},
+{ id: 15, name: 'Kumara', reo_name: 'Kūmara'},
+{ id: 16, name: 'Lettuce', reo_name: 'Rētihi'},
+{ id: 17, name: 'Mandarins', reo_name: 'Manarini'},
+{ id: 18, name: 'Mushrooms', reo_name: 'Harore'},
+{ id: 19, name: 'Onions', reo_name: 'Riki'},
+{ id: 20, name: 'Oranges', reo_name: 'Ārani'},
+{ id: 21, name: 'Parsnips', reo_name: 'Tāmore mā'},
+{ id: 22, name: 'Pears', reo_name: 'Pea'},
+{ id: 23, name: 'Pineapple', reo_name: 'Paināporo'},
+{ id: 24, name: 'Potatoes', reo_name: 'Rīwai'},
+{ id: 25, name: 'Pumpkin', reo_name: 'Paukena'},
+{ id: 26, name: 'Tomatoes', reo_name: 'Tōmato'}
 ])
 
