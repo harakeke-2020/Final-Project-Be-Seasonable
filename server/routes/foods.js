dad11343067c3a84ba125a718550cca91c5208a7
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-02-27 17:36:24 +1300

    update eslint and webpack

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -6,8 +6,7 @@ const db = require('../db/db')
 router.get('/:month', (req, res) => {
   const month = Number(req.params.month)
   return db.getInSeasonFoods(month)
-  .then(foods => res.status(200).json(foods))
+    .then(foods => res.status(200).json(foods))
 })
-
 
 module.exports = router
