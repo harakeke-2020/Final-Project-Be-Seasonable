8e4dcec66fcd71421edd3f864e2e06f6922ed80d
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-02-27 16:36:30 +1300

    add tests for get in season foods reducer

---
+++ client/reducers/getInSeasonFoodsReducer.test.js
@@ -0,0 +1,33 @@
+import season from './getInSeasonFoodsReducer'
+
+describe('Get in season food reducer tests', () => {
+  it('GET_IN_SEASON_FOODS_SUCCESS returns foods', () => {
+    const testAction = {
+      type: 'GET_IN_SEASON_FOODS_SUCCESS',
+      foods: [
+        {
+          id: 1,
+          name: 'Apples',
+          reoName: 'Āporo',
+          price: 3.67,
+          image: ''
+        },
+        {
+          id: 2,
+          name: 'Pears',
+          reoName: 'Pea',
+          price: 4.56,
+          image: ''
+        }
+      ]
+    }
+    expect(season([], testAction)[0].name).toBe('Apples')
+  })
+
+  it('Default returns []', () => {
+    const testAction = {
+      type: 'hi'
+    }
+    expect(season([], testAction)).toMatch([])
+  })
+})
