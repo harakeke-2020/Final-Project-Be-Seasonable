00c669a397cde1b411f4228e0bd10a3d87fbd629
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-02-28 11:09:08 +1300

    add extra route tests for error handling

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -6,7 +6,16 @@ const db = require('../db/db')
 router.get('/:month', (req, res) => {
   const month = Number(req.params.month)
   return db.getInSeasonFoods(month)
-    .then(foods => res.status(200).json(foods))
+    .then(foods => {
+      if (foods === undefined) {
+        res.sendStatus(404)
+      } else {
+        res.json(foods)
+      }
+    })
+    .catch(() => {
+      res.status(500).send('We had a problem getting in season foods. You can try refreshing the page.')
+    })
 })
 
 module.exports = router
