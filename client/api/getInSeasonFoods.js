668eb25786851cd2dd9287b920a155fcdddac79f
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-02-27 13:52:28 +1300

    minor linting fix

--- client/api/getInSeasonFoods.js
+++ client/api/getInSeasonFoods.js
@@ -2,8 +2,8 @@ import request from 'superagent'
 
 const apiURL = '/api/v1/foods/'
 
-export function getInSeasonFoods(month) {
-  return request 
+export function getInSeasonFoods (month) {
+  return request
     .get(`${apiURL}${month}`)
     .then(res => res.body)
 }
