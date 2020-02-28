be32943c334a326e0c858091adf7db85f493f1eb
Brendan Olphert <57166341+brendan-olphert@users.noreply.github.com>
2020-02-29 11:05:49 +1300

    update css

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <section>
-      <h1 data-testid={'header'}>Be Seasonable</h1>
-      <p>Be reasonable, shop seasonable</p>
-    </section>
+    <header>
+      <p>🍅 Be Seasonable</p>
+      <h1 data-testid={'header'}>Shop seasonal</h1>
+      <h2>Save money, save the planet</h2>
+      <h2 data-testid={'foodlistheading'}>In season now:</h2>
+    </header>
   )
 }
 
