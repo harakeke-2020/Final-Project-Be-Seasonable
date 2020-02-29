b162ec517171cdc765738e98418efc4451e84740
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-02-29 20:11:02 +1300

    add maori dictionary credit

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <p data-testid={'footer'}><a href="https://figure.nz/table/jt7dAiBlCdqDtzcT">Food price data</a> sourced from Figure.NZ under CC-BY license</p>
+      <p>Te Reo Māori names sourced from the <a href="https://maoridictionary.co.nz/">Māori Dictionary</a></p>
     </footer>
   )
 }
