8bfaa83374c2fb84330e01d95f3487c1b3a3560f
Lorenzo Fernandez <57726755+lorenzo-joseph-fernandez-19@users.noreply.github.com>
2020-03-01 12:20:13 +1300

    created test for foodDetails route

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -18,15 +18,19 @@ router.get('/:month', (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:month/:id', (req, res) => {
+  const month = Number(req.params.month)
   const id = Number(req.params.id)
-  return db.getFoodDetails(id)
+  return db.getFoodDetails(month, id)
     .then(details => {
       if (details === undefined) {
         res.sendStatus(404)
       } else {
         res.json(details)
       }
+    })
+    .catch(() => {
+      res.status(500).send('We had a problem getting food details. You can try refreshing the page.')
     })
 })
 
