bd9b2fa602a10faed59856a5ebc1e677b17a11a4
Qianye Lin <52944295+0qy@users.noreply.github.com>
2020-02-29 15:40:15 +1300

    Add last and first month

--- client/components/FoodItem.jsx
+++ client/components/FoodItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Flag from './Flag'
+
 const FoodItem = (props) => {
   return (
     <article data-aos="fade-up" >
@@ -7,6 +9,10 @@ const FoodItem = (props) => {
         <h3 data-testid={'foodItem'}>{props.food.reoName}</h3>
         <p>{props.food.name}</p>
         <p>${props.food.price}</p>
+        {
+          props.food.nzGrown === 0 &&
+            <Flag message={'Imported year round -- high carbon footprint.'} />
+        }
       </div>
       <div>
         <img data-aos="fade-left" data-aos-delay="100" src='https://clipartart.com/images/apple-bitten-clipart-18.png'/>
@@ -15,4 +21,12 @@ const FoodItem = (props) => {
   )
 }
 
-export default FoodItem
+  const lastMonthTrue = () => {
+    const date = new Date()
+    const month = date.getMonth() + 1
+    
+    
+
+  }
+
+export default connect(mapStateToProps)(FoodItem)
--- server/db/db.js
+++ server/db/db.js
@@ -6,7 +6,7 @@ function getInSeasonFoods (month, db = connection) {
   return db('foods')
     .join('prices', 'foods.id', 'prices.food_id')
     .where('prices.month', month)
-    .select('foods.id as id', 'foods.name', 'foods.reo_name as reoName', 'prices.month', 'prices.price', 'foods.image', 'foods.nz_grown as nzGrown')
+    .select('foods.id as id', 'foods.name', 'foods.reo_name as reoName', 'prices.month', 'prices.price', 'foods.image', 'foods.nz_grown as nzGrown', 'foods.last_month as lastMonth', 'foods.first_month as firstMonth')
 }
 
 module.exports = {
