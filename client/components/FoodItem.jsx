1f3b048e7efa493c9f1453ff7e4c74397c2183ea
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-02-29 16:06:54 +1300

    add better font layouts

--- client/components/FoodItem.jsx
+++ client/components/FoodItem.jsx
@@ -4,9 +4,8 @@ const FoodItem = (props) => {
   return (
     <article data-aos="fade-up" >
       <div>
-        <h3 data-testid={'foodItem'}>{props.food.reoName}</h3>
-        <p>{props.food.name}</p>
-        <p>${props.food.price}</p>
+        <span><h3 data-testid={'foodItem'}>{props.food.reoName}</h3>   <h3>{props.food.name}</h3></span>
+        <span><h3>${props.food.price}</h3> <h3>/ kg</h3></span>
       </div>
       <div>
         <img data-aos="fade-left" data-aos-delay="100" src='https://clipartart.com/images/apple-bitten-clipart-18.png'/>
