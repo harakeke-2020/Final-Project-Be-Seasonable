7ee5a196762fb309b6a49dc7a85aef2f04386ff2
0qy <linqianye333@gmail.com>
2020-03-01 12:28:47 +1300

    added monthRedecer and tests passing

---
+++ client/reducers/monthReducer.js
@@ -0,0 +1,21 @@
+import {
+  NEXT_MONTH,
+  LAST_MONTH,
+  CURRENT_MONTH
+} from '../actions/monthAction'
+
+const date = new Date()
+const month = date.getMonth() + 1
+
+export default function monthReducer (action, state = month) {
+  switch (action.type) {
+    case NEXT_MONTH:
+      return action.month
+    case LAST_MONTH:
+      return action.month
+    case CURRENT_MONTH:
+      return action.month
+    default:
+      return state
+  }
+}
