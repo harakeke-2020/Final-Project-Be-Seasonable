2a75c06fbf5c0e073fabbd331764ff0a652f4503
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-03-01 12:21:51 +1300

    add about component

---
+++ client/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About () {
+  return (
+    <div className="container">
+      <h1>About Be Seasonable</h1>
+      <h2>What is it?</h2>
+      <p>Be seasonable is a free website that tells you what produce is currently in season, the average monthly cost based on the Statistics NZ Food Price Index, and any ethical warnings we've identified for that produce.</p>
+      <p>It does this by combining the average monthly costs of in-season produce with research into the environmental and ethical impacts of production.</p>
+      <h2>Why did you make it?</h2>
+      <p>Be Seasonable is designed to help you make informed decisions about your produce purchases.</p>
+      <p>Even when you care deeply about ethical, affordable eating, it can be hard to make good choices. We wanted to make it easy to make informed decisions.</p>
+      <p>Fruit and vegetables are great for our health, but they can be hard on the wallet and hard on the planet.</p>
+      <p>Produce that is imported can have a high carbon cost, contributing to global warming. It may be farmed in areas using intensive irrigation, increasing the water usage. Or, the industry may have unethical labour practices and treat their employees poorly.</p>
+      <h2>Who is behind it?</h2>
+      <p>Be Seasonable was made by <a href="https://github.com/0qy">Qian-Ye</a>, <a href="https://github.com/lorenzo-joseph-fernandez-19">Lorenzo</a>, <a href="https://github.com/brendan-olphert">Brendan</a>, <a href="https://github.com/H-THO">Hywel</a>, and <a href="https://github.com/natdudley-EDA">Nat</a> as our graduation project for <a href="https://devacademy.co.nz/">Ensipiral Dev Academy</a>.</p>
+    </div>
+  )
+}
+
+export default About
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,12 +6,14 @@ import Footer from './Footer'
 import FoodList from './FoodList'
 import ErrorComponent from './Error'
 import Waiting from './Waiting'
+import About from './About'
 
 const App = () => {
   return (
     <Router>
-      <Route path='/' component={Header}/>
-      <Route path='/' component={FoodList}/>
+      <Route exact path='/' component={Header}/>
+      <Route exact path='/' component={FoodList}/>
+      <Route path='/about' component={About}/>
       <Route path='/' component={ErrorComponent} />
       <Route path='/' component={Waiting} />
       <Route path='/' component={Footer}/>
