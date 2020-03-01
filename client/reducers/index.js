abcd97053a0f659e6dd4fe85ebc6e358e6efef6c
Brendan Olphert <57166341+brendan-olphert@users.noreply.github.com>
2020-03-01 16:26:29 +1300

    added foodDetails reducer to combineReducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import foodList from './getInSeasonFoodsReducer'
 import error from './error'
 import waiting from './waiting'
+import details from './getFoodDetailsReducer'
 
 export default combineReducers({
   foodList,
   error,
-  waiting
+  waiting,
+  details
 })
