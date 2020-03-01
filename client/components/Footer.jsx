29325b5d6b0c8ba331659dd2549641ac898bb1ad
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-03-01 17:09:48 +1300

    add photo credit to footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <footer>
       <p data-testid={'footer'}><a href="https://figure.nz/table/jt7dAiBlCdqDtzcT">Food price data</a> sourced from Figure.NZ under CC-BY license</p>
       <p>Te Reo Māori names sourced from the <a href="https://maoridictionary.co.nz/">Māori Dictionary</a></p>
+      <p>Photo by <a href="https://unsplash.com/@dosejuice">Dose Juice</a></p>
     </footer>
   )
 }
