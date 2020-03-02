123420d01a69cc5f89185082f328ff8d4113127d
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-03-02 13:18:26 +1300

    fix label

--- client/components/FoodItem.jsx
+++ client/components/FoodItem.jsx
@@ -16,7 +16,7 @@ const FoodItem = (props) => {
         <div>
           <span><h3 data-testid={'foodItem'}>{props.food.reoName}</h3>   <h3>{props.food.name}</h3></span>
           <div className="prices">
-            <span><label>10-year average price</label></span>
+            <span><label>Average monthly price</label></span>
             <span><h3>${props.food.price}</h3> <h3>/ kg</h3></span>
           </div>
           {
