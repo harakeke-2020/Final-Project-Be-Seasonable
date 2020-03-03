a19a4892f80dcd2fe53dc8cb0c1ac7344b61d72a
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-03-04 08:59:34 +1300

    update components for video

--- client/components/Error.jsx
+++ client/components/Error.jsx
@@ -4,9 +4,9 @@ import { connect } from 'react-redux'
 function ErrorComponent (props) {
   return props.error
     ? <div data-testid={'error'} style={{ margin: '100px', textAlign: 'center' }}>
-      <h2>Bananas! We had a problem</h2>
+      <h2>Oops. We slipped up!</h2>
       <p>{props.error}</p>
-      <img style={{ width: '200px' }} src='/waiting.gif' alt='🌶️ HOLD UP BUDDY 🌶️' />
+      <img style={{ width: '200px' }} src='/bananas.png' alt='🌶️ HOLD UP BUDDY 🌶️' />
     </div>
     : null
 }
