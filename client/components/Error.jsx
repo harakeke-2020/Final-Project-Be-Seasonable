da115f201ec95c90a860615654c4712c836dd157
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-03-03 16:55:48 +1300

    change error text color

--- client/components/Error.jsx
+++ client/components/Error.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 function ErrorComponent (props) {
   return props.error
-    ? <div data-testid={'error'} style={{ margin: '100px', textAlign: 'center', color: 'red' }}>
+    ? <div data-testid={'error'} style={{ margin: '100px', textAlign: 'center' }}>
       <h2>Bananas! We had a problem</h2>
       <p>{props.error}</p>
       <img style={{ width: '200px' }} src='/waiting.gif' alt='🌶️ HOLD UP BUDDY 🌶️' />
