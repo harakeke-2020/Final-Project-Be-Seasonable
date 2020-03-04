18165969693a0f2375f85dae13b95302ed4fe0e4
Nat Dudley <natalie.dudley+EDA@gmail.com>
2020-03-04 14:19:15 +1300

    service worker

--- client/index.js
+++ client/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
+import * as OfflinePluginRuntime from 'offline-plugin/runtime'
 
 import App from './components/App'
 import reducers from './reducers'
+OfflinePluginRuntime.install()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
